98fe05c152df16cd5b41d0023637afc2652956a8
Paula POP <paula.pop@ens.uvsq.fr>
2021-02-08 15:13:42 +0100

    Ajout infos du groupe

--- projet_incendie.py
+++ projet_incendie.py
@@ -7,4 +7,25 @@
 # Quentin ASSIE
 # Paula POP
 # https://github.com/uvsq22001770/projet_incendie.git
+
 #########################################
+# import des librairies
+
+#########################################
+# définition des constantes
+
+#########################################
+# définition des variables globales
+
+#########################################
+# définition des fonctions
+# docstring pour chaque fonction
+
+#########################################
+# programme principal
+
+#########################################
+# définition des widgets 
+
+#########################################
+# définition des évènements
