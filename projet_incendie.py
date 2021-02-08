be34809b84cfe041c1fe943e1c8de4a499555832
Adrien HANNA <adrien.hanna@ens.uvsq.fr>
2021-02-08 16:47:08 +0100

    couleurs aléatoires

---
+++ projet_incendie.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+import random
+HAUTEUR = 700
+LONGUEUR = 1000
+
+racine=tk.Tk()
+canvas = tk.Canvas(racine, bg="grey", height=HAUTEUR, width=LONGUEUR)
+
+canvas_eau=tk.Canvas(racine, bg="blue", height=100, width=100)
+canvas_foret=tk.Canvas(racine, bg="green", height=100, width=100)
+canvas_cendres=tk.Canvas(racine, bg="black", height=100, width=100)
+canvas_feu=tk.Canvas(racine, bg="red", height=100, width=100)
+canvas_prairie=tk.Canvas(racine, bg="yellow", height=100, width=100)
+canvas_cendrestiedes=tk.Canvas(racine, bg="grey", height=100, width=100)
+
+def couleur():
+    x = random.randint(0,2)
+    if x == 0:
+        return "blue"
+    elif x == 1:
+        return "yellow"
+    elif x == 2:
+        return "green"       
+
+
+for i in range(0, 10):
+    for j in range(0, 8):
+        canvas.create_rectangle((i*100, j*100), (i*100 + 100, j*100 - 100), fill= couleur())
+
+
+"""canvas_eau.grid(column=, row=)
+canvas_foret.grid(column=, row=)
+canvas_cendres.grid(column= , row=)
+canvas_feu.grid(column= , row=)
+canvas_prairie.grid(column= , row=)
+canvas_cendrestiedes.grid(column= , row=)"""
+
+
+
+canvas.grid()
+
+
+racine.mainloop()
